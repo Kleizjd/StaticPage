628d8c0f5dc6241ddebf586ebf343ec9c2796529
Jose Daniel Grijalba Osorio <jjdd1997@hotmail.com>
2020-05-14 23:43:09 -0500

    add database real

--- psicoapp_web.sql
+++ psicoapp_web.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.1
+-- version 5.0.1
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 15-05-2020 a las 02:06:11
--- Versión del servidor: 10.4.8-MariaDB
--- Versión de PHP: 7.3.11
+-- Tiempo de generación: 15-05-2020 a las 06:36:51
+-- Versión del servidor: 10.4.11-MariaDB
+-- Versión de PHP: 7.4.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -53,11 +53,28 @@ CREATE TABLE `rol` (
 --
 
 CREATE TABLE `usuario` (
+  `id_persona` int(11) NOT NULL,
   `correo` varchar(20) COLLATE utf8mb4_spanish2_ci NOT NULL,
-  `contraseña` varchar(20) COLLATE utf8mb4_spanish2_ci NOT NULL,
-  `codigo_persona` int(20) NOT NULL,
-  `id_rol` int(20) NOT NULL
+  `contrasena` varchar(20) COLLATE utf8mb4_spanish2_ci NOT NULL,
+  `id_rol` int(20) NOT NULL,
+  `nombres` varchar(50) COLLATE utf8mb4_spanish2_ci DEFAULT NULL,
+  `apellidos` varchar(50) COLLATE utf8mb4_spanish2_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`id_persona`, `correo`, `contrasena`, `id_rol`, `nombres`, `apellidos`) VALUES
+(1, 'prueba2@hotmail.com', '1234', 1, NULL, NULL),
+(5, 'prueba@hotmail.com', '1234', 1, NULL, NULL),
+(7, 'prueb@hotmail.com', '1234', 1, NULL, NULL),
+(8, 'prue@hotmail.com', '1234', 1, NULL, NULL),
+(9, 'pru@hotmail.com', '1234', 1, NULL, NULL),
+(15, 'ru@hotmail.com', '1234', 1, NULL, NULL),
+(16, 'qw@hot.com', 'qwe', 1, NULL, NULL),
+(18, 'wer@h.com', '123', 1, 'pdre', 'car'),
+(19, 'asd@hotmail.com', '123', 1, 'cristhian', 'hernandez');
 
 --
 -- Índices para tablas volcadas
@@ -73,18 +90,18 @@ ALTER TABLE `persona`
 -- Indices de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  ADD PRIMARY KEY (`correo`),
-  ADD KEY `codigo_persona` (`codigo_persona`);
+  ADD PRIMARY KEY (`id_persona`),
+  ADD UNIQUE KEY `correo` (`correo`);
 
 --
--- Restricciones para tablas volcadas
+-- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
--- Filtros para la tabla `usuario`
+-- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  ADD CONSTRAINT `usuario_ibfk_1` FOREIGN KEY (`codigo_persona`) REFERENCES `persona` (`cod_persona`);
+  MODIFY `id_persona` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
